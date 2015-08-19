7ec5df9ffe4b52f98510f1dc25c17086dd6fb38b
Xavier MARIN <marin.xavier@gmail.com>
2015-08-19 21:27:03 +0200

    documentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,36 +4,38 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var wrench = require('wrench');
 var gulpNgConfig = require('gulp-ng-config');
+var plato = require('gulp-plato');
+var jsdoc = require("gulp-jsdoc");
 
 var options = {
-  src: 'src',
-  dist: 'dist',
-  tmp: '.tmp',
-  e2e: 'e2e',
-  errorHandler: function(title) {
-    return function(err) {
-      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
-      this.emit('end');
-    };
-  },
-  wiredep: {
-    directory: 'bower_components'
-  }
+    src: 'src',
+    dist: 'dist',
+    tmp: '.tmp',
+    e2e: 'e2e',
+    errorHandler: function (title) {
+        return function (err) {
+            gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+            this.emit('end');
+        };
+    },
+    wiredep: {
+        directory: 'bower_components'
+    }
 };
 
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file)(options);
+wrench.readdirSyncRecursive('./gulp').filter(function (file) {
+    return (/\.(js|coffee)$/i).test(file);
+}).map(function (file) {
+    require('./gulp/' + file)(options);
 });
 
 gulp.task('conf-dev', function () {
-  gulp.src('conf/config.json')
-      .pipe(gulpNgConfig('qaobee.config', {
-          environment: 'local',
-          wrap: true
-      }))
-      .pipe(gulp.dest('./src/app'))
+    gulp.src('conf/config.json')
+        .pipe(gulpNgConfig('qaobee.config', {
+            environment: 'local',
+            wrap: true
+        }))
+        .pipe(gulp.dest('./src/app'))
 });
 
 gulp.task('conf-prod', function () {
@@ -47,4 +49,22 @@ gulp.task('conf-prod', function () {
 
 gulp.task('default', ['clean', 'conf-prod'], function () {
     gulp.start('build');
+});
+
+gulp.task('jsdoc', [], function () {
+    return gulp.src(options.src + '/**/*.js')
+        .pipe(jsdoc('./docs/jsdoc'))
+});
+gulp.task('plato', [], function () {
+    return gulp.src(options.src + '/**/*.js')
+        .pipe(plato('docs/plato', {
+            jshint: {
+                options: {
+                    strict: true
+                }
+            },
+            complexity: {
+                trycatch: true
+            }
+        }));
 });
--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m' # No Color
+echo -e "${green}****************************************************"
+echo -e "Clean"
+echo -e "****************************************************${NC}"
 rm -rf dist/*
+echo -e "${green}****************************************************"
+echo -e "Running build"
+echo -e "****************************************************${NC}"
 gulp
-surge dist www.qaobee.com
+STATUS=$?
+    if [ $STATUS -eq 0 ]; then
+    echo -e "${green}****************************************************"
+    echo -e "Build ok, processing openshift deployment"
+    echo -e "****************************************************${NC}"
+    surge dist www.qaobee.com
+    echo -e "${green}****************************************************"
+    echo -e "Deploy doc"
+    echo -e "****************************************************${NC}"
+    gulp plato
+    gulp jsdoc
+    lftp ftp://heber_15054748:zaza666@ftp.hebergratuit.net -e "mirror -e -R --parallel=20 --only-newer --verbose docs /htdocs/qswarm-web/docs; quit"
+ else
+    echo -e "${red}****************************************************"
+    echo -e "Build Failed"
+    echo -e "****************************************************${NC}"
+fi
+
