55ca86fabb96da5693a386ccd0717ddf7f528518
Christophe Kervella <ch.kervella@gmail.com>
2015-11-25 23:41:27 +0100

    Efficacité tir

--- src/app/modules/sandbox/stats/player/playerStats.js
+++ src/app/modules/sandbox/stats/player/playerStats.js
@@ -45,9 +45,7 @@
             $scope.playerId = $routeParams.playerId;
             $scope.periodicity = 'month';
             $scope.periodicityActive = {};
-            $scope.chartData = [{data:0}];
-
-                $scope.effCol = [{id: 'data', type: 'gauge'}];
+            
             // return button
             $scope.doTheBack = function () {
                 $window.history.back();
@@ -55,6 +53,18 @@
 
             //Initialization owner Object
             $scope.player = {};
+            $scope.efficaciteGlobalCol = [{id: 'data', type: 'gauge', color:'#42a5f5'}];
+            $scope.efficaciteGlobalData = [{data:0}];
+            
+            $scope.efficacite9mCol = [{id: 'data', type: 'gauge', color:'#42a5f5'}];
+            $scope.efficacite9mData = [{data:0}];
+        
+            $scope.efficacite7mCol = [{id: 'data', type: 'gauge', color:'#42a5f5'}];
+            $scope.efficacite7mData = [{data:0}];
+        
+            $scope.efficacite6mCol = [{id: 'data', type: 'gauge', color:'#42a5f5'}];
+            $scope.efficacite6mData = [{data:0}];
+
 
             /* get player */
             $scope.getPlayer = function () {
@@ -71,6 +81,7 @@
                 var ownersId = [];
                 ownersId.push(ownerId);
 
+                //Efficiently global
                 var search = {
                     listIndicators: ['goalScored', 'originShootAtt'],
                     listOwners: ownersId,
@@ -82,11 +93,126 @@
 
                 statsRestAPI.getStatGroupBy(search).success(function (data) {
                     var efficaciteGlobal = 0;
+                    $scope.efficaciteGlobalCol = [{id: 'data', type: 'gauge', color: '#42a5f5'}];
+                    $scope.efficaciteGlobalData = [{data:0}];
+                    
                     if (angular.isDefined(data[0]) && data !== null) {
                         efficaciteGlobal = (data[0].value / data[1].value) * 100;
-                        $log.debug('etape 1', efficaciteGlobal)
+                        $log.debug('global', efficaciteGlobal);
+                        if (efficaciteGlobal<25) {
+                            $scope.efficaciteGlobalCol[0].color = '#ef5350';
+                        } else if(efficaciteGlobal>=25 && efficaciteGlobal<50) {
+                            $scope.efficaciteGlobalCol[0].color = '#ffb74d';
+                        } else if(efficaciteGlobal>=50 && efficaciteGlobal<75) {
+                            $scope.efficaciteGlobalCol[0].color = '#42a5f5';
+                        } else if(efficaciteGlobal>75) {
+                            $scope.efficaciteGlobalCol[0].color = '#66bb6a';
+                        }
+                            
+                        $scope.efficaciteGlobalData.push({data : efficaciteGlobal});
                     }
-                    $scope.chartData.push({data : efficaciteGlobal});
+                });
+                
+                //Efficiently 9m
+                //TODO : CKE : Virer le code en dur
+                var search = {
+                    listIndicators: ['originShootAtt'],
+                    listOwners: ownersId,
+                    startDate: startDate.valueOf(),
+                    endDate: endDate.valueOf(),
+                    values: ['bottom-right', 'bottom-center', 'bottom-left'],
+                    aggregat: 'COUNT',
+                    listFieldsGroupBy: ['owner', 'code']
+                };
+
+                statsRestAPI.getStatGroupBy(search).success(function (data) {
+                    var efficacite9m = 0;
+                    $scope.efficacite9mCol = [{id: 'data', type: 'gauge', color: '#42a5f5'}];
+                    $scope.efficacite9mData = [{data:0}];
+                    
+                    if (angular.isDefined(data[0]) && data !== null) {
+                        efficacite9m = (data[0].value / 10) * 100;
+                        $log.debug('9m', efficacite9m);
+                        if (efficacite9m<25) {
+                            $scope.efficacite9mCol[0].color = '#ef5350';
+                        } else if(efficacite9m>=25 && efficacite9m<50) {
+                            $scope.efficacite9mCol[0].color = '#ffb74d';
+                        } else if(efficacite9m>=50 && efficacite9m<75) {
+                            $scope.efficacite9mCol[0].color = '#42a5f5';
+                        } else if(efficacite9m>75) {
+                            $scope.efficacite9mCol[0].color = '#66bb6a';
+                        }
+                            
+                        $scope.efficacite9mData.push({data : efficacite9m});
+                    }
+                });
+                
+                //Efficiently 6m
+                //TODO : CKE : Virer le code en dur
+                var search = {
+                    listIndicators: ['originShootAtt'],
+                    listOwners: ownersId,
+                    startDate: startDate.valueOf(),
+                    endDate: endDate.valueOf(),
+                    values: ['top-right', 'top-center', 'top-left'],
+                    aggregat: 'COUNT',
+                    listFieldsGroupBy: ['owner', 'code']
+                };
+
+                statsRestAPI.getStatGroupBy(search).success(function (data) {
+                    var efficacite6m = 0;
+                    $scope.efficacite6mCol = [{id: 'data', type: 'gauge', color: '#42a5f5'}];
+                    $scope.efficacite6mData = [{data:0}];
+                    
+                    if (angular.isDefined(data[0]) && data !== null) {
+                        efficacite6m = (data[0].value / 10) * 100;
+                        $log.debug('6m', efficacite6m);
+                        if (efficacite6m<25) {
+                            $scope.efficacite6mCol[0].color = '#ef5350';
+                        } else if(efficacite6m>=25 && efficacite6m<50) {
+                            $scope.efficacite6mCol[0].color = '#ffb74d';
+                        } else if(efficacite6m>=50 && efficacite6m<75) {
+                            $scope.efficacite6mCol[0].color = '#42a5f5';
+                        } else if(efficacite6m>75) {
+                            $scope.efficacite6mCol[0].color = '#66bb6a';
+                        }
+                            
+                        $scope.efficacite6mData.push({data : efficacite6m});
+                    }
+                });
+                
+                //Efficiently 7m
+                //TODO : CKE : Virer le code en dur
+                var search = {
+                    listIndicators: ['originShootAtt'],
+                    listOwners: ownersId,
+                    startDate: startDate.valueOf(),
+                    endDate: endDate.valueOf(),
+                    values: ['PENALTY'],
+                    aggregat: 'COUNT',
+                    listFieldsGroupBy: ['owner', 'code']
+                };
+
+                statsRestAPI.getStatGroupBy(search).success(function (data) {
+                    var efficacite7m = 0;
+                    $scope.efficacite7mCol = [{id: 'data', type: 'gauge', color: '#42a5f5'}];
+                    $scope.efficacite7mData = [{data:0}];
+                    
+                    if (angular.isDefined(data[0]) && data !== null) {
+                        efficacite7m = (data[0].value / 10) * 100;
+                        $log.debug('7m', efficacite7m);
+                        if (efficacite7m<25) {
+                            $scope.efficacite7mCol[0].color = '#ef5350';
+                        } else if(efficacite7m>=25 && efficacite7m<50) {
+                            $scope.efficacite7mCol[0].color = '#ffb74d';
+                        } else if(efficacite7m>=50 && efficacite7m<75) {
+                            $scope.efficacite7mCol[0].color = '#42a5f5';
+                        } else if(efficacite7m>75) {
+                            $scope.efficacite7mCol[0].color = '#66bb6a';
+                        }
+                            
+                        $scope.efficacite7mData.push({data : efficacite7m});
+                    }
                 });
             };
 
