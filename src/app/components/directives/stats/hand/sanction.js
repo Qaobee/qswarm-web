a2c55de8e7a037d6d8db72bbbd3587b524443fcc
Xavier MARIN <marin.xavier@gmail.com>
2016-02-13 17:17:34 +0100

    sonar

--- src/app/components/directives/stats/hand/sanction.js
+++ src/app/components/directives/stats/hand/sanction.js
@@ -12,86 +12,87 @@
 
     angular.module('statsSanction', ['statsSRV', 'statsRestAPI', 'qaobee.eventbus'])
 
-    .directive('statsSanction', function ($translatePartialLoader, $log, $q, $filter, statsRestAPI, statsSrv, qeventbus) {
-        return {
-            restrict: 'E',
-            
-            controller: function ($scope) {
-                $translatePartialLoader.addPart('stats');
-                $scope.noStat = true;
+        .directive('statsSanction', function ($translatePartialLoader, $log, $q, $filter, statsRestAPI, statsSrv, qeventbus) {
+            return {
+                restrict: 'E',
 
-                /* Refresh widget on periodicity change */
-                $scope.$on('qeventbus', function () {
-                    if ("periodicityActive" === qeventbus.message) {
-                        $scope.startDate = qeventbus.data.startDate;
-                        $scope.endDate = qeventbus.data.endDate;
-                        $scope.ownersId = qeventbus.data.ownersId;
-                        buildWidget();
-                    }
-                });
-                
-                /* getStats */  
-                var getStats = function (ownersId, startDate, endDate, values) {
-                    var deferred = $q.defer(); 
-                    var search = {};
-                    var result = {
-                        nbYellowCard : 0,
-                        nbExclTmp : 0,
-                        nbRedCard : 0
+                controller: function ($scope) {
+                    $translatePartialLoader.addPart('stats');
+                    $scope.noStat = true;
+
+                    /* getStats */
+                    var getStats = function (ownersId, startDate, endDate /* , values */) {
+                        var deferred = $q.defer();
+                       // var search = {};
+                        var result = {
+                            nbYellowCard: 0,
+                            nbExclTmp: 0,
+                            nbRedCard: 0
+                        };
+
+                        var indicators = Array.create('yellowCard', 'exclTmp', 'redCard');
+                        var listFieldsGroupBy = Array.create('owner', 'code');
+
+                        var search = {
+                            listIndicators: indicators,
+                            listOwners: ownersId,
+                            startDate: startDate.valueOf(),
+                            endDate: endDate.valueOf(),
+                            aggregat: 'COUNT',
+                            listFieldsGroupBy: listFieldsGroupBy
+                        };
+
+                        /* Appel stats API */
+                        statsRestAPI.getStatGroupBy(search).success(function (data) {
+                            if (angular.isArray(data) && data.length > 0) {
+                                $scope.noStat = true;
+                                data.forEach(function (a) {
+                                    if (a._id.code === 'yellowCard') {
+                                        result.nbYellowCard = a.value;
+                                    }
+                                    if (a._id.code === 'exclTmp') {
+                                        result.nbExclTmp = a.value;
+                                    }
+                                    if (a._id.code === 'redCard') {
+                                        result.nbRedCard = a.value;
+                                    }
+                                });
+                                deferred.resolve(result);
+                            } else {
+                                $scope.noStat = false;
+                                deferred.reject('getStats -> problem for : ' + search);
+                            }
+                        });
+
+                        return deferred.promise;
                     };
-                    
-                    var indicators =  Array.create('yellowCard', 'exclTmp', 'redCard');
-                    var listFieldsGroupBy = Array.create('owner', 'code');
 
-                    var search = {
-                        listIndicators: indicators,
-                        listOwners: ownersId,
-                        startDate: startDate.valueOf(),
-                        endDate: endDate.valueOf(),
-                        aggregat: 'COUNT',
-                        listFieldsGroupBy: listFieldsGroupBy
+                    var buildWidget = function () {
+                        $scope.nbYellowCard = 0;
+                        $scope.nbExclTmp = 0;
+                        $scope.nbRedCard = 0;
+
+                        $scope.title = 'stats.resumeTab.' + $scope.label;
+
+                        getStats($scope.ownersId, $scope.startDate, $scope.endDate, $scope.values).then(function (result) {
+                            $scope.nbYellowCard = result.nbYellowCard;
+                            $scope.nbExclTmp = result.nbExclTmp;
+                            $scope.nbRedCard = result.nbRedCard;
+                        });
                     };
 
-                    /* Appel stats API */
-                    statsRestAPI.getStatGroupBy(search).success(function (data) {
-                        if (angular.isArray(data) && data.length > 0) {
-                            $scope.noStat = true;
-                            data.forEach(function(a){
-                                if(a._id.code === 'yellowCard'){
-                                    result.nbYellowCard = a.value;
-                                }
-                                if(a._id.code === 'exclTmp'){
-                                    result.nbExclTmp = a.value;
-                                }
-                                if(a._id.code === 'redCard'){
-                                    result.nbRedCard = a.value;
-                                }
-                            });
-                            deferred.resolve(result);  
-                        } else {
-                            $scope.noStat = false;
-                            deferred.reject('getStats -> problem for : ' + search);
+                    /* Refresh widget on periodicity change */
+                    $scope.$on('qeventbus', function () {
+                        if ("periodicityActive" === qeventbus.message) {
+                            $scope.startDate = qeventbus.data.startDate;
+                            $scope.endDate = qeventbus.data.endDate;
+                            $scope.ownersId = qeventbus.data.ownersId;
+                            buildWidget();
                         }
-                    })
-
-                    return deferred.promise;
-                }; 
-                
-                var buildWidget= function (){
-                    $scope.nbYellowCard = 0;
-                    $scope.nbExclTmp = 0;
-                    $scope.nbRedCard = 0;
-                    
-                    $scope.title = 'stats.resumeTab.'+$scope.label;
-                    
-                    getStats($scope.ownersId, $scope.startDate, $scope.endDate, $scope.values).then(function (result) {
-                        $scope.nbYellowCard = result.nbYellowCard;
-                        $scope.nbExclTmp = result.nbExclTmp;
-                        $scope.nbRedCard = result.nbRedCard;
                     });
-                }
-            },
-            templateUrl: 'app/components/directives/stats/hand/sanction.html'
-        };
-    });
+
+                },
+                templateUrl: 'app/components/directives/stats/hand/sanction.html'
+            };
+        });
 }());
