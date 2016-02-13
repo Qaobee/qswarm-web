98a962eb2f7fe595e53cbec88d522562f95423e4
Xavier MARIN <marin.xavier@gmail.com>
2016-02-13 17:02:03 +0100

    sonar

--- src/app/modules/sandbox/stats/effective/effectiveStats.js
+++ src/app/modules/sandbox/stats/effective/effectiveStats.js
@@ -11,64 +11,61 @@
      * @requires {@link qaobee.components.restAPI.sandbox.effective.personRestAPI|qaobee.components.restAPI.sandbox.effective.personRestAPI}
      */
     angular.module('qaobee.effectiveStats', [
-        
-        /* qaobee Rest API */
-        'effectiveRestAPI',
-        'personRestAPI',
-        'userRestAPI',
-        
-        /* qaobee widget */
-    ])
+            /* qaobee Rest API */
+            'effectiveRestAPI',
+            'personRestAPI',
+            'userRestAPI'
+            /* qaobee widget */
+        ])
 
-    .config(function ($routeProvider, metaDatasProvider) {
-        $routeProvider.when('/private/effectiveStats/:effectiveId', {
-            controller: 'EffectiveStats',
-            resolve: {
-                user: metaDatasProvider.checkUser,
-                meta: metaDatasProvider.getMeta
-            },
-            templateUrl: 'app/modules/sandbox/stats/effective/effectiveStats.html'
+        .config(function ($routeProvider, metaDatasProvider) {
+            $routeProvider.when('/private/effectiveStats/:effectiveId', {
+                controller: 'EffectiveStats',
+                resolve: {
+                    user: metaDatasProvider.checkUser,
+                    meta: metaDatasProvider.getMeta
+                },
+                templateUrl: 'app/modules/sandbox/stats/effective/effectiveStats.html'
 
+            });
+        })
+        /**
+         * @class qaobee.modules.home.HomeControler
+         */
+        .controller('EffectiveStats', function ($log, $scope, $routeParams, $window, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta,
+                                                effectiveRestAPI, personRestAPI, userRestAPI) {
+            $translatePartialLoader.addPart('home');
+            $translatePartialLoader.addPart('stats');
+
+            $scope.user = user;
+            $scope.meta = meta;
+            $scope.effectiveId = $routeParams.effectiveId;
+
+            // return button
+            $scope.doTheBack = function () {
+                $window.history.back();
+            };
+
+            $scope.effective = {};
+            /* Retrieve list effective */
+            $scope.getEffective = function () {
+
+                effectiveRestAPI.getEffective($scope.effectiveId).success(function (data) {
+                    $scope.effective = data;
+                });
+            };
+
+            /* check user connected */
+            $scope.checkUserConnected = function () {
+
+                userRestAPI.getUserById(user._id).success(function (/* data */) {
+                    $scope.getEffective();
+                }).error(function (/* data */) {
+                    $log.error('EffectiveStats : User not Connected');
+                });
+            };
+
+            /* Primary, check if user connected */
+            $scope.checkUserConnected();
         });
-    })
-/**
- * @class qaobee.modules.home.HomeControler
- */
-    .controller('EffectiveStats', function ($log, $scope, $routeParams, $window, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta, 
-                                            effectiveRestAPI, personRestAPI, userRestAPI) {
-        $translatePartialLoader.addPart('home');
-        $translatePartialLoader.addPart('stats');
-
-        $scope.user = user;
-        $scope.meta = meta;
-        $scope.effectiveId = $routeParams.effectiveId;
-        
-        // return button
-        $scope.doTheBack = function() {
-            $window.history.back();
-        };
-        
-        $scope.effective = {};
-        
-        /* Retrieve list effective */
-        $scope.getEffective = function () {
-            
-            effectiveRestAPI.getEffective($scope.effectiveId).success(function (data) {
-                $scope.effective = data;
-            });
-        };                     
-        
-        /* check user connected */
-        $scope.checkUserConnected = function () {
-            
-            userRestAPI.getUserById(user._id).success(function (data) {
-                $scope.getEffective();
-            }).error(function (data) {
-                $log.error('EffectiveStats : User not Connected');
-            });
-        }; 
-        
-        /* Primary, check if user connected */
-        $scope.checkUserConnected();
-    });
 }());
