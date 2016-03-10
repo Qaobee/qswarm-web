47005a1af72687cc374ab7d4858cd750dfa25f14
Xavier MARIN <marin.xavier@gmail.com>
2016-03-10 22:28:58 +0100

    lint

--- src/app/modules/sandbox/effective/mainEffective.js
+++ src/app/modules/sandbox/effective/mainEffective.js
@@ -17,25 +17,25 @@
      * @copyright <b>QaoBee</b>.
      */
     angular.module('qaobee.effective', [
-        /* angular qaobee */
-        'ngAutocomplete',
-        
-        /* qaobee modules */
-        'qaobee.addEffective',
-        'qaobee.updateEffective',
-        'qaobee.addPlayer',
-        'qaobee.updatePlayer',
-        'qaobee.viewPlayer',
-        'qaobee.addTeam',
-        'qaobee.updateTeam',
-        
-        /* qaobee services */
-        'effectifSRV',
-        
-        /* qaobee Rest API */
-        'effectiveRestAPI',
-        'teamRestAPI',
-        'userRestAPI'])
+            /* angular qaobee */
+            'ngAutocomplete',
+
+            /* qaobee modules */
+            'qaobee.addEffective',
+            'qaobee.updateEffective',
+            'qaobee.addPlayer',
+            'qaobee.updatePlayer',
+            'qaobee.viewPlayer',
+            'qaobee.addTeam',
+            'qaobee.updateTeam',
+
+            /* qaobee services */
+            'effectifSRV',
+
+            /* qaobee Rest API */
+            'effectiveRestAPI',
+            'teamRestAPI',
+            'userRestAPI'])
 
 
         .config(function ($routeProvider, metaDatasProvider) {
@@ -51,112 +51,112 @@
             });
         })
 
-    /**
-     * @class qaobee.modules.sandbox.effective.MainEffectiveControler
-     * @description Main controller for view mainEffective.html
-     */
-        .controller('MainEffectiveControler', function ($log, $scope, $routeParams, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta, 
-                                                         effectiveRestAPI, effectiveSrv, teamRestAPI, userRestAPI, $timeout) {
+        /**
+         * @class qaobee.modules.sandbox.effective.MainEffectiveControler
+         * @description Main controller for view mainEffective.html
+         */
+        .controller('MainEffectiveControler', function ($log, $scope, $routeParams, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta,
+                                                        effectiveRestAPI, effectiveSrv, teamRestAPI, userRestAPI, $timeout) {
 
-        $translatePartialLoader.addPart('effective');
-        $translatePartialLoader.addPart('stats');
-        $translatePartialLoader.addPart('commons');
-        
-        $scope.effectiveId = $routeParams.effectiveId;
-        $scope.user.effectiveDefault = $scope.effectiveId;
+            $translatePartialLoader.addPart('effective');
+            $translatePartialLoader.addPart('stats');
+            $translatePartialLoader.addPart('commons');
 
-        $scope.user = user;
-        $scope.meta = meta;
-        $scope.players = [];
-        $scope.effectives = [];
-        $scope.currentEffective = {};
-        $scope.currentCategory = null;
-        $scope.listTeamHome = {};
-        
-        
-        /* Retrieve list of team of effective */
-        $scope.getListTeamHome = function () {
-            teamRestAPI.getListTeamHome($scope.meta.sandbox._id, $scope.effectiveId, 'all').success(function (data) {
-                $scope.listTeamHome = data.sortBy(function(n) {
-                        return n.label; 
+            $scope.effectiveId = $routeParams.effectiveId;
+            $scope.user.effectiveDefault = $scope.effectiveId;
+
+            $scope.user = user;
+            $scope.meta = meta;
+            $scope.players = [];
+            $scope.effectives = [];
+            $scope.currentEffective = {};
+            $scope.currentCategory = null;
+            $scope.listTeamHome = {};
+
+
+            /* Retrieve list of team of effective */
+            $scope.getListTeamHome = function () {
+                teamRestAPI.getListTeamHome($scope.meta.sandbox._id, $scope.effectiveId, 'all').success(function (data) {
+                    $scope.listTeamHome = data.sortBy(function (n) {
+                        return n.label;
                     });
-            });
-        };
-        
-        /* Retrieve list effective */
-        $scope.getEffectives = function () {
-            
-            effectiveRestAPI.getListEffective($scope.meta._id, $scope.currentCategory).success(function (data) {
-                $scope.effectives = data.sortBy(function(n) {
-                    return n.label; 
                 });
-                
-                /* retrieve the current effective */ 
-                data.forEach(function (a) {
-                    if(a._id===$scope.effectiveId) {
-                        $scope.currentEffective = a;
-                    } 
-                });
-                
-                $scope.getEffective();
-            });
-        };                                                                                                
+            };
 
-        /* Retrieve current effective and list player */
-        $scope.getEffective = function () { 
-            effectiveSrv.getEffective($scope.user.effectiveDefault).then(function(data){
-                $scope.currentEffective = data;
-                
-                effectiveSrv.getListId($scope.currentEffective, 'player').then(function(listId){
-                    var listField = Array.create('_id', 'name', 'firstname', 'avatar', 'status', 'birthdate', 'contact');
-                    
-                    effectiveSrv.getPersons(listId, listField).then(function(players){
-                        $scope.players = players;
-                        $scope.players.forEach(function (e) {
-                            if (angular.isDefined(e.status.positionType)) {
-                                e.positionType = $filter('translate')('stats.positionType.value.' + e.status.positionType);
-                            } else {
-                                e.positionType = '';
-                            }
-                            
-                            e.birthdate = $filter('date')(e.birthdate, 'yyyy');
-                            e.age = moment().format('YYYY') - e.birthdate;
+            /* Retrieve list effective */
+            $scope.getEffectives = function () {
+
+                effectiveRestAPI.getListEffective($scope.meta._id, $scope.currentCategory).success(function (data) {
+                    $scope.effectives = data.sortBy(function (n) {
+                        return n.label;
+                    });
+
+                    /* retrieve the current effective */
+                    data.forEach(function (a) {
+                        if (a._id === $scope.effectiveId) {
+                            $scope.currentEffective = a;
+                        }
+                    });
+
+                    $scope.getEffective();
+                });
+            };
+
+            /* Retrieve current effective and list player */
+            $scope.getEffective = function () {
+                effectiveSrv.getEffective($scope.user.effectiveDefault).then(function (data) {
+                    $scope.currentEffective = data;
+
+                    effectiveSrv.getListId($scope.currentEffective, 'player').then(function (listId) {
+                        var listField = Array.create('_id', 'name', 'firstname', 'avatar', 'status', 'birthdate', 'contact');
+
+                        effectiveSrv.getPersons(listId, listField).then(function (players) {
+                            $scope.players = players;
+                            $scope.players.forEach(function (e) {
+                                if (angular.isDefined(e.status.positionType)) {
+                                    e.positionType = $filter('translate')('stats.positionType.value.' + e.status.positionType);
+                                } else {
+                                    e.positionType = '';
+                                }
+
+                                e.birthdate = $filter('date')(e.birthdate, 'yyyy');
+                                e.age = moment().format('YYYY') - e.birthdate;
+                            });
                         });
                     });
-                });  
-            });                                                                                      
-        };
-        
-        /* keep in memory tab by default */
-        $scope.changeTabDefault = function (tabId) {
-            user.effectiveTabId = tabId;
-        }
-        
-        /* check user connected */
-        $scope.checkUserConnected = function () {
-            
-            userRestAPI.getUserById(user._id).success(function (data) {
-                
-                $scope.getEffectives();
-                $scope.getListTeamHome();
-                $timeout(function() {
-                    if(user.effectiveTabId){
-                        $('ul.tabs').tabs('select_tab', user.effectiveTabId);
-                    } else {
-                        $('ul.tabs').tabs('select_tab', 'playerList');
-                    }
-                }, 100);
-            }).error(function (data) {
-                $log.error('MainEffectiveControler : User not Connected');
-            });
-        }; 
-        
-        /* Primary, check if user connected */
-        $scope.checkUserConnected();
-        
-        
-    })
-    //
+                });
+            };
+
+            /* keep in memory tab by default */
+            $scope.changeTabDefault = function (tabId) {
+                user.effectiveTabId = tabId;
+            }
+
+            /* check user connected */
+            $scope.checkUserConnected = function () {
+
+                userRestAPI.getUserById(user._id).success(function (data) {
+
+                    $scope.getEffectives();
+                    $scope.getListTeamHome();
+                    $timeout(function () {
+                        if (user.effectiveTabId) {
+                            $('ul.tabs').tabs('select_tab', user.effectiveTabId);
+                        } else {
+                            $('ul.tabs').tabs('select_tab', 'playerList');
+                        }
+                    }, 100);
+                }).error(function (data) {
+                    $log.error('MainEffectiveControler : User not Connected');
+                });
+            };
+
+            /* Primary, check if user connected */
+            $scope.checkUserConnected();
+
+
+        })
+        //
     ;
 }());
 
