9da3e06d9782f071adcc4092174c96d057051278
Xavier MARIN <marin.xavier@gmail.com>
2016-03-10 22:25:47 +0100

    lint

--- src/app/modules/sandbox/stats/mainStats.js
+++ src/app/modules/sandbox/stats/mainStats.js
@@ -11,76 +11,76 @@
      * @requires {@link qaobee.components.restAPI.sandbox.effective.personRestAPI|qaobee.components.restAPI.sandbox.effective.personRestAPI}
      */
     angular.module('qaobee.stats', [
-        
-        /* qaobee Rest API */
-        'effectiveRestAPI',
-        'eventsRestAPI',
-        'personRestAPI',
-        'userRestAPI',
-        
-        /* qaobee module */
-        'qaobee.effectiveStats',
-        'qaobee.eventStats',
-        'qaobee.playerStats',
-        'qaobee.teamStats',
-        
-        /* c3 angular directive */
-        'gridshore.c3js.chart',
-        
-        /* qaobee widget */
-        'statsEfficiency',
-        'statsGoals',
-        'statsPlayerUse',
-        'statsSanction',
-        'statsActionsPosNeg'
-    ])
 
-    .config(function ($routeProvider, metaDatasProvider) {
-        $routeProvider.when('/private/mainStats', {
-            controller: 'MainStats',
-            resolve: {
-                user: metaDatasProvider.checkUser,
-                meta: metaDatasProvider.getMeta
-            },
-            templateUrl: 'app/modules/sandbox/stats/mainStats.html'
+            /* qaobee Rest API */
+            'effectiveRestAPI',
+            'eventsRestAPI',
+            'personRestAPI',
+            'userRestAPI',
 
-        });
-    })
-/**
- * @class qaobee.modules.home.HomeControler
- */
-    .controller('MainStats', function ($log, $scope, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta, 
-                                            effectiveRestAPI, personRestAPI, eventsRestAPI, userRestAPI) {
-        $translatePartialLoader.addPart('home');
-        $translatePartialLoader.addPart('stats');
+            /* qaobee module */
+            'qaobee.effectiveStats',
+            'qaobee.eventStats',
+            'qaobee.playerStats',
+            'qaobee.teamStats',
 
-        $scope.user = user;
-        $scope.meta = meta;
-        
-        $scope.showGraph = function() {
-            $scope.chart = c3.generate({
+            /* c3 angular directive */
+            'gridshore.c3js.chart',
+
+            /* qaobee widget */
+            'statsEfficiency',
+            'statsGoals',
+            'statsPlayerUse',
+            'statsSanction',
+            'statsActionsPosNeg'
+        ])
+
+        .config(function ($routeProvider, metaDatasProvider) {
+            $routeProvider.when('/private/mainStats', {
+                controller: 'MainStats',
+                resolve: {
+                    user: metaDatasProvider.checkUser,
+                    meta: metaDatasProvider.getMeta
+                },
+                templateUrl: 'app/modules/sandbox/stats/mainStats.html'
+
+            });
+        })
+        /**
+         * @class qaobee.modules.home.HomeControler
+         */
+        .controller('MainStats', function ($log, $scope, $translatePartialLoader, $location, $rootScope, $q, $filter, user, meta,
+                                           effectiveRestAPI, personRestAPI, eventsRestAPI, userRestAPI) {
+            $translatePartialLoader.addPart('home');
+            $translatePartialLoader.addPart('stats');
+
+            $scope.user = user;
+            $scope.meta = meta;
+
+            $scope.showGraph = function () {
+                $scope.chart = c3.generate({
                     bindto: '#chart',
                     data: {
-                      columns: [
-                        ['data1', 30, 200, 100, 400, 150, 250],
-                        ['data2', 50, 20, 10, 40, 15, 25]
-                      ]
+                        columns: [
+                            ['data1', 30, 200, 100, 400, 150, 250],
+                            ['data2', 50, 20, 10, 40, 15, 25]
+                        ]
                     }
-                });     
-        }
-        
-        
-        /* check user connected */
-        $scope.checkUserConnected = function () {
-            
-            userRestAPI.getUserById(user._id).success(function (data) {
-                $scope.showGraph();
-            }).error(function (data) {
-                $log.error('MainStats : User not Connected');
-            });
-        }; 
-        
-        /* Primary, check if user connected */
-        $scope.checkUserConnected();
-    });
+                });
+            };
+
+
+            /* check user connected */
+            $scope.checkUserConnected = function () {
+
+                userRestAPI.getUserById(user._id).success(function (data) {
+                    $scope.showGraph();
+                }).error(function (data) {
+                    $log.error('MainStats : User not Connected');
+                });
+            };
+
+            /* Primary, check if user connected */
+            $scope.checkUserConnected();
+        });
 }());
