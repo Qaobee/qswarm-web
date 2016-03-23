e0bb7e62d5fa59abc948849f1d9ebc7cfbd13882
Xavier MARIN <marin.xavier@gmail.com>
2016-03-24 00:16:49 +0100

    Notifications

--- src/app/components/directives/commons/headerMenu/headerMenu.js
+++ src/app/components/directives/commons/headerMenu/headerMenu.js
@@ -194,14 +194,14 @@
                                     $log.debug('socket connected');
                                     eb.registerHandler('qaobee.notification.' + $scope.user._id, function (message) {
                                         if(!!message.title) {
-                                            toastr.info(message.title, message.content.stripTags().truncate(30));
+                                            toastr.info(message.content.stripTags().truncate(30),message.title);
                                         }
                                         qeventbus.prepForBroadcast('notifications', message);
                                         getNotifications();
                                     });
                                     eb.registerHandler('qaobee.notification.' + $rootScope.meta.sandbox._id, function (message) {
                                         if(!!message.title) {
-                                            toastr.info(message.title, message.content.stripTags().truncate(30));
+                                            toastr.info(message.content.stripTags().truncate(30),message.title);
                                         }
                                         qeventbus.prepForBroadcast('notifications', message);
                                         getNotifications();
