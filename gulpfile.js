65f943abb73b838ce0fb9bdd6ca56717963075fa
Xavier MARIN <marin.xavier@gmail.com>
2016-04-20 22:27:14 +0200

    sonar

--- gulpfile.js
+++ gulpfile.js
@@ -85,11 +85,11 @@ gulp.task('sonar', function () {
             host: {
                 url: 'http://flufy.hd.free.fr/sonarqube'
             },
-          /*  jdbc: {
+            jdbc: {
                 url: 'jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance',
                 username: 'sonar',
                 password: 'sonar'
-            }, */
+            },
             projectKey: 'com.qaobee:qswarm-web:1.0.0',
             projectName: 'QSwarmWeb',
             projectVersion: '1.0.0',
@@ -105,13 +105,13 @@ gulp.task('sonar', function () {
             exec: {
                 // All these properties will be send to the child_process.exec method (see: https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback )
                 // Increase the amount of data allowed on stdout or stderr (if this value is exceeded then the child process is killed, and the gulp-sonar will fail).
-                maxBuffer : 1024*1024
+                maxBuffer: 1024 * 1024
             }
         }
     };
 
     // gulp source doesn't matter, all files are referenced in options object above
-    return gulp.src('thisFileDoesNotExist.js', { read: false })
+    return gulp.src('thisFileDoesNotExist.js', {read: false})
         .pipe(sonar(options))
         .on('error', util.log);
 });
