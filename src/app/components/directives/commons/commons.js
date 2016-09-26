b6402ae799ee98c0e19119aa924b23315a18217d
Xavier MARIN <marin.xavier@gmail.com>
2016-09-26 23:35:02 +0200

    #mant0000060: [EFF-003] Ajout joueur : poids non entier

--- src/app/components/directives/commons/commons.js
+++ src/app/components/directives/commons/commons.js
@@ -97,6 +97,28 @@
             };
         })
 
+        .directive('numbersOnly', function () {
+            return {
+                require: 'ngModel',
+                link: function (scope, element, attr, ngModelCtrl) {
+                    function fromUser(text) {
+                        if (text) {
+                            var transformedInput = text.replace(/[^0-9]/g, '');
+
+                            if (transformedInput !== text) {
+                                ngModelCtrl.$setViewValue(transformedInput);
+                                ngModelCtrl.$render();
+                            }
+                            return transformedInput;
+                        }
+                        return undefined;
+                    }
+
+                    ngModelCtrl.$parsers.push(fromUser);
+                }
+            };
+        })
+
 //
     ;
 }());
