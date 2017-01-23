641f78f318140313ca0e26462e338a380a87cb2d
Xavier MARIN <marin.xavier@gmail.com>
2017-01-23 21:12:51 +0100

    bug fix + changelog

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,17 +1,29 @@
-#!/bin/bash
-# Generates changelog day by day
+#!/usr/bin/env bash
+# Generates changelog tag by tag
 NEXT=$(date +%F)
 echo "# CHANGELOG"
 echo "---"
-git log --format="%cd" --date=short | sort -u -r | head -n 20 | while read DATE ; do
-    echo
-    echo '## '$DATE
-    git log --date=iso --format="> +  %s %N (*par [%cN](%ce) à ts%ct [view commit](https://gitlab.com/qaobee/qaobee-swarn/commit/%H)*)" --since=$DATE --until=$NEXT | while read LOG; do
-        BEGIN=$(echo $LOG | sed -rn 's/(^.*)\ ts[0-9]{10}\ .*$/\1/p')
-        TIMESTAMP=@$(echo $LOG | sed -rn 's/^.*\ ts([0-9]{10})\ .*$/\1/p')
-        TIME=$(date -d $TIMESTAMP +%H:%M)
-        END=$(echo $LOG | sed -rn 's/^.*\ ts[0-9]{10}\ (.*)$/\1/p')
-        echo "$BEGIN $TIME $END"
-    done
-    NEXT=$DATE
+
+git tag | sort -u -r | head -n 20 | while read TAG ; do
+    if [ -z ${LAST_TAG+x} ];
+        then
+            TAG_QUERY="...$TAG";
+        else
+            TAG_QUERY="$TAG...$LAST_TAG";
+        fi
+        echo
+        echo '## '$TAG
+        git log --date=iso --format="> +  ts%ct  | %s %N (*par [%cN](%ce) [view commit](https://gitlab.com/qaobee/QSwarm/commit/%H)*)" ${TAG_QUERY} | while read LOG; do
+            BEGIN=$(echo $LOG | sed -rn 's/(^.*)\ ts[0-9]{10}\ .*$/\1/p')
+            TIMESTAMP=@$(echo $LOG | sed -rn 's/^.*\ ts([0-9]{10})\ .*$/\1/p')
+            TIME=$(date -d $TIMESTAMP +'%x %H:%M')
+            END=$(echo $LOG | sed -rn 's/^.*\ ts[0-9]{10}\ (.*)$/\1/p')
+            JIRA=$(echo $END | sed -rn 's/(QHBWEB-[0-9]+)/[\1](https:\/\/qaobee.atlassian.net\/browse\/\1)/p')
+            if [ -n "$JIRA" ]; then
+                echo "$BEGIN $TIME $JIRA";
+            else
+                echo "$BEGIN $TIME $END";
+            fi
+        done
+    LAST_TAG=$TAG
 done
