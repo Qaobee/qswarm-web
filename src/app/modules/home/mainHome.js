011f2efca0519c5959ff427c471d60f5b5b469b5
Xavier MARIN <marin.xavier@gmail.com>
2017-10-16 22:24:44 +0200

    First connexion wizard i18n

--- src/app/modules/home/mainHome.js
+++ src/app/modules/home/mainHome.js
@@ -28,7 +28,9 @@
         /**
          * @class qaobee.modules.home.HomeControler
          */
-        .controller('HomeControler', function ($scope, $rootScope, $translatePartialLoader, user, meta, effectiveSrv, effectiveRestAPI, profileRestAPI, sandboxRestAPI, qeventbus, filterCalendarSrv, $timeout, $log) {
+        .controller('HomeControler', function ($scope, $rootScope, $translatePartialLoader, user, meta, effectiveSrv,
+                                               effectiveRestAPI, profileRestAPI, sandboxRestAPI, qeventbus, $filter,
+                                               filterCalendarSrv, $timeout, $log) {
             $translatePartialLoader.addPart('home').addPart('stats').addPart('agenda').addPart('effective');
             $scope.user = user;
             $scope.meta = meta;
@@ -63,36 +65,56 @@
             };
 
             $scope.IntroOptions = {
-                steps:[
-                {
-                    element: '#step1',
-                    intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>Bienvenue sur Qaobee !</b></div><div class="card-title center"><br> <i class="fa fa-fw fa-star fa-2x accent-text-color"></i><br>Prenons quelques instants pour découvrir les principales fonctionnalités de l\'application.<br>&nbsp;</div></div></div>'
-                },
-                {
-                    element: '#step2',
-                    intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>Restez à l\'affût</b> </span></div><div class="card-title center"><br><i class="fa fa-bell fa-2x accent-text-color"></i> <br>Les notifications .... <br>&nbsp;</div></div></div>',
-                    position: 'bottom'
-                },
-                {
-                    element: '#step3',
-                    intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>Planifiez vos matchs !</b></span></div><div class="card-title center"><br> <i class="fa fa-fw fa-calendar fa-2x accent-text-color"></i> <br>Les événements .... <br>&nbsp;</div></div></div>',
-                    position: 'bottom'
-                },
-                {
-                    element: '#step4',
-                    intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>Consultez vos stats !</b> </span></div><div class="card-title center"><br><i class="fa fa-fw fa-bar-chart fa-2x accent-text-color"></i> <br>Les collectes .... <br>&nbsp;</div></div></div>',
-                    position: 'bottom'
-                },
-                {
-                    element: '#step5',
-                    intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>Félicitations !</b></span></div><div class="card-title center"><br> <i class="fa fa-fw fa-star fa-2x accent-text-color"></i> <br>Bientôt vous serez incollable sur les stats de vos joueurs <br>&nbsp;</div><div class="card-title center"><br> Rendez-vous sur Google play store et téléchargez notre application de collecte pour tablette ou smartphone.<br><a href="https://play.google.com/store/apps/details?id=com.qaobee.hand" target="_blank" class="light-text-color" rel="noopener"> <img src="assets/images/googleplay.png" class="responsive-img" style="width: 15rem;" alt="play store logo"/></a><br>&nbsp;</div></div></div>'
-                }
+                steps: [
+                    {
+                        element: '#step1',
+                        intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>' +
+                        $filter('translate')('intro.step1.title') +
+                        '</b></div><div class="card-title center"><br><i class="fa fa-fw fa-star fa-2x accent-text-color"></i><br>' +
+                        $filter('translate')('intro.step1.desc') + '<br>&nbsp;</div></div></div>'
+                    },
+                    {
+                        element: '#step2',
+                        intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>' +
+                        $filter('translate')('intro.step2.title') +
+                        '</b></span></div><div class="card-title center"><br><i class="fa fa-bell fa-2x accent-text-color"></i><br>' +
+                        $filter('translate')('intro.step2.desc') + '<br>&nbsp;</div></div></div>',
+                        position: 'bottom'
+                    },
+                    {
+                        element: '#step3',
+                        intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>' +
+                        $filter('translate')('intro.step3.title') +
+                        '</b></span></div><div class="card-title center"><br><i class="fa fa-fw fa-calendar fa-2x accent-text-color"></i><br>' +
+                        $filter('translate')('intro.step3.desc') + '<br>&nbsp;</div></div></div>',
+                        position: 'bottom'
+                    },
+                    {
+                        element: '#step4',
+                        intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>' +
+                        $filter('translate')('intro.step4.title') +
+                        '</b></span></div><div class="card-title center"><br><i class="fa fa-fw fa-bar-chart fa-2x accent-text-color"></i><br>' +
+                        $filter('translate')('intro.step4.desc') + '<br>&nbsp;</div></div></div>',
+                        position: 'bottom'
+                    },
+                    {
+                        element: '#step5',
+                        intro: '<div class="row"><div class="card col s12"><div class="card-title dark-primary-color center-align"><b>' +
+                        $filter('translate')('intro.step5.title') +
+                        '</b></span></div><div class="card-title center"><br><i class="fa fa-fw fa-star fa-2x accent-text-color"></i><br>' +
+                        $filter('translate')('intro.step5.desc') +
+                        '<br>&nbsp;</div><div class="card-title center"><br>' +
+                        $filter('translate')('intro.step5.desc2') +
+                        '<br><a href="https://play.google.com/store/apps/details?id=com.qaobee.hand" target="_blank" class="light-text-color" rel="noopener">' +
+                        '<img src="assets/images/googleplay.png" class="responsive-img" style="width: 15rem;" alt="play store logo"/></a>' +
+                        '<br>&nbsp;</div></div></div>'
+                    }
                 ],
                 showStepNumbers: false,
                 showBullets: false,
                 exitOnOverlayClick: true,
-                exitOnEsc:true,
-                showProgress:true,
+                exitOnEsc: true,
+                showProgress: true,
                 nextLabel: '<span>Suivant</span>',
                 prevLabel: '<span>Précédent</span>',
                 skipLabel: 'Annuler',
@@ -104,7 +126,7 @@
              *
              * @returns {boolean}
              */
-            $scope.launchIntroJs = function (){
+            $scope.launchIntroJs = function () {
                 return $scope.user.account.firstConnexion;
             };
             /**
@@ -134,6 +156,6 @@
                     $scope.periodicity = filterCalendarSrv.getValue().periodicity;
                     $scope.getEffective($scope.meta.sandbox.effectiveDefault);
                 }
-            },0);
+            }, 0);
         });
 }());
